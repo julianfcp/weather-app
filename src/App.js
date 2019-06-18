57063f713d684b0c02c0c7416af42b18e55de030
Julian <julian.fcp@gmail.com>
2019-06-18 01:19:09 -0500

    FlexBox y MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import LocationList from './components/LocationList';
 
@@ -18,12 +24,37 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <AppBar position="sticky">
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList 
+                cities={cities} 
+                onSelectionClickWL={this.handleSelectionClickWL}>
+              </LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <div className="details"></div>
+            </Col>
+          </Row>
+        </Grid>
+
+      {/*<div className="App">
         <LocationList 
           cities={cities} 
           onSelectionClickWL={this.handleSelectionClickWL}>
         </LocationList>
-      </div>
+    </div>*/}
+    </MuiThemeProvider>
     );
   }
 }
